dafe319f5e64878f102c664bb9bb3b5284208661
_rocketman <85868026+2Kelvin@users.noreply.github.com>
2023-12-13 12:14:24 +0300

    cleanup tweaking

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 export default function Footer() {
     return (
         <footer>
-            <small>Built by Kelvin Njihia</small><br />
+            <small>Built by Kelvin Mwangi</small><br />
             <small>rocketmancodes02@gmail.com</small><br />
             <small>{new Date().getFullYear()}</small>
             <div className='socialIconsDiv'>
